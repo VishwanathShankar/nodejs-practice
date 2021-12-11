b984f36721d355bcb8cc565c66322432943371f4
Vishwanath <vishwa.s787@gmail.com>
2021-12-11 10:10:05 +0530

    Test Code

---
+++ Models/City.js
@@ -0,0 +1,18 @@
+class City {
+    name;
+    city_id,
+    location_id,
+    country_name
+
+    City(name, city_id, location_id, country_name )
+}
+
+City c1 = new City("bangalore" , "1", "2", "india");
+
+
+City (
+    name: String,
+    id,
+    location_id,
+    country_name
+)
